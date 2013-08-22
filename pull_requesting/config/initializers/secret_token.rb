38cde4103f767fda2c9a878804d1a48690b1900f
Michael Swanson <mikeswanson12@msn.com>
2013-08-21 21:33:42 -0400

    initial commit

---
+++ pull_requesting/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PullRequesting::Application.config.secret_key_base = 'bdae798e2c3d3ac4ec78f69efb38d0867d0a3a924ccba6ce5819e45f47578096063aa59f6a4380ba3fb8c586b97a5d5e4952029db3ebe803d8498050f79e0f55'
